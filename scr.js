4d0529a38ddac2d7479e9a09773cd9f526cba392
Irena Daneva <de.luxe9997@gmail.com>
2023-02-01 14:11:36 +0200

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,31 @@
+function tu()
+{
+    window.location.href='https://www.tu-plovdiv.bg/en/';
+}
+function pgi()
+{
+    window.location.href='https://pgi-smolyan.info/';
+}
+function c()
+{
+    window.location.href='https://learn.microsoft.com/en-us/cpp/c-language/?view=msvc-170';
+}
+function java()
+{
+    window.location.href='https://docs.oracle.com/en/java/';
+}
+function py()
+{
+    window.location.href='https://www.python.org/doc/';
+}
+function sql()
+{
+    window.location.href='https://learn.microsoft.com/en-us/sql/?view=sql-server-ver16';
+}
+function htcs()
+{
+    window.location.href='https://developer.mozilla.org/en-US/';
+}function js()
+{
+    window.location.href='https://developer.mozilla.org/en-US/docs/Web/javascript';
+}
